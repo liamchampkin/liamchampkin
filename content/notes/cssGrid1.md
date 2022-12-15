5b611287b51a03763672f296fa43d87e27b35384
Liam Champkin <liamchampkin@gmail.com>
2022-12-15 21:49:59 +0000

    new css grid post

---
+++ content/notes/cssGrid1.md
@@ -0,0 +1,45 @@
+---
+title: 12 column grid ends at 13
+type: post
+id: 'grid'
+date: 2022-12-15
+category: notes
+tags: css
+color: hsl(181deg 44% 38%)
+---
+
+A 12 column CSS grid starts at 1 and ends at 13 and that has always tripped me up when writing CSS until I searched online to try and understand why.
+
+So a 12 column grid would have 13 lines starting at one.
+
+e.g. To make an element span across all 12 grid columns you'd have to specify the start line and end very last line...
+
+  ```css
+    grid-column: 1 / 13;
+  ```
+But this has always tripped me up as I'd write...
+
+  ```css
+    grid-column: 0 / 12;
+  ```
+
+And then...
+
+  ```css
+    grid-column: 1 / 12;
+  ```
+
+And wonder why it didn't work.
+
+JavaScript and a lot of other programming languages start at 0 so it only felt right to me that CSS grid started there too. Writing 13 instead of 12 felt like a hack that somehow worked.
+
+But come to think of it, CSS psuedo classes start at 1, e.g...
+
+  ```css
+  /* Target the first paragraph */
+    p:nth-of-type(1) {
+      font-weight: bold;
+    }
+  ```
+
+And that makes sense so maybe it's just a CSS convention. To think of columns as tracks with edges also helps me form a mental model of the grid. The twelth column starts at 12 and ends at 13 for example.
---
+++ layouts/articles.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <Header />
+  <main>
+    <div class="content-container">
+      <header class="page-heading">
+        <div class="wrapper">
+          <h1>{{ title }}</h1>
+          <p class="font-medium text-lg">{{ desc }}</p>
+        </div>
+      </header>
+      <section class="posts ">
+        <!-- Render list of all articles in ./content/blog using `path` -->
+        <!-- Provide only defined fields in the `:query` prop -->
+        <ContentList :query="query">
+          <!-- Default list slot -->
+          <template v-slot="{ list }">
+            <ul class="article-list grid-12">
+              <li v-for="article in list" :key="article._path" class="article">
+                <NuxtLink :to="article._path">
+                  <div class="card">
+                    <div class="banner" :style="{ 'background-color': article.color }">
+                      <h2>{{ article.title }}</h2>
+                    </div>
+                    <div class="card-content">
+                      <p>{{ article.description }}</p>
+                      <!-- <ul class="article-tags">
+                        <li class="tag !py-0.5" v-for="(tag, n) in article.tags" :key="n">{{ tag }}</li>
+                      </ul> -->
+                    </div>
+
+
+                  </div>
+                </NuxtLink>
+              </li>
+            </ul>
+          </template>
+
+          <!-- slot to display message when no content is found -->
+          <template #not-found>
+            <p>No articles found.</p>
+          </template>
+        </ContentList>
+      </section>
+    </div>
+    <div class="nightshine"></div>
+  </main>
+  <Footer />
+
+</template>
+<style scoped>
+/* ... */
+</style>
